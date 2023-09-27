e783c8b5f99d5c752d089c9d08ce3cb9adf9da02
PioBuczek <piotrbuczek49@gmail.com>
2023-09-27 20:56:35 +0200

    cmd

--- client/client.py
+++ client/client.py
@@ -8,8 +8,33 @@ BUFFER = 1024
 client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
+
+def authentication(client_socket):
+    authenticated = False
+    while not authenticated:
+        print("Enter your username: ")
+        username = input()
+        print("Enter your password: ")
+        password = input()
+        client_socket.send(username.encode("utf8"))
+        client_socket.send(password.encode("utf8"))
+        response = client_socket.recv(BUFFER).decode("utf8")
+        if response.startswith("Hi"):
+            server_answer_authenticator = client_socket.recv(BUFFER).decode("utf8")
+            print(server_answer_authenticator)
+            authenticated = True
+
+        else:
+            print(response)
+    server_answer = client_socket.recv(BUFFER).decode("utf8")
+    print(server_answer)
+    print("skonczyłem autoryzacje")
+    print("zaczynamy")
+
+
+authentication(client_socket)
+
 while True:
-    print("What you want: ")
-    client_socket.send(input().encode("utf8"))
+    client_socket.send(input("Enter your command: ").encode("utf8"))
     server_answer = client_socket.recv(BUFFER).decode("utf8")
     print(server_answer)
--- client/server.py
+++ client/server.py
@@ -1,3 +1,91 @@
+# import json
+# import socket as s
+# import datetime
+
+# HOST = "192.168.0.115"
+# PORT = 33000
+# BUFFER = 1024
+# uptime = datetime.datetime.now()
+# help_msg = "uptime - returns time of server activity\ninfo - returns version of the server and issue date\nhelp - returns list of commands\nstop - terminates connection\n"
+# info = "version 1.0.0, issue date 13.12.2022\n"
+# stop = "connection terminated\n"
+# possible_options = ["uptime", "help_msg", "info", "stop"]
+
+# server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+# server_socket.bind((HOST, PORT))
+# server_socket.listen(2)
+# client_socket, address = server_socket.accept()
+
+
+# def open_user():
+#     with open("database.json", "r") as file:
+#         data = json.load(file)
+#         return data.get("users", [])
+
+
+# users = open_user()
+
+
+# def authentication(client_socket):
+#     authenticated_user = None
+#     while authenticated_user is None:
+#         user_request = client_socket.recv(BUFFER).decode("utf8")
+#         password_request = client_socket.recv(BUFFER).decode("utf8")
+
+#         for user in users:
+#             if (
+#                 user["username"] == user_request
+#                 and user["password"] == password_request
+#             ):
+#                 authenticated_user = user
+#                 break
+
+#         if authenticated_user:
+#             client_socket.send(f"Hi {user_request} !".encode("utf8"))
+#             return authenticated_user["type"]
+#         else:
+#             client_socket.send("Something is wrong, try again".encode("utf8"))
+
+
+# authenticated_user_type = authentication(client_socket)
+
+# if authenticated_user_type:
+#     client_socket.send(f"You are connected as {authenticated_user_type}".encode("utf8"))
+#     print(f"You are connected as {authenticated_user_type}")
+#     client_socket.send("Enter your command".encode("utf8"))
+# server_answer = ""
+
+# # ...
+# while True:
+#     print("Waiting for a connection...")
+#     client_socket, address = server_socket.accept()
+#     print(f"Call received from {address[0]}:{address[1]}")
+
+#     authenticated_user_type = authentication(client_socket)
+
+#     if authenticated_user_type:
+#         client_socket.send(
+#             f"You are connected as {authenticated_user_type}".encode("utf8")
+#         )
+#         print(f"You are connected as {authenticated_user_type}")
+#         client_socket.send("Enter your command".encode("utf8"))
+
+#     while True:
+#         client_request = client_socket.recv(BUFFER).decode("utf8")
+#         if client_request == "info":
+#             server_answer = info
+#         elif client_request == "help_msg":
+#             server_answer = help_msg
+#         elif client_request == "uptime":
+#             server_answer = str(datetime.datetime.now() - uptime)
+#         elif client_request == "stop":
+#             client_socket.send(stop.encode("utf8"))
+#             client_socket.close()
+#             break
+
+#         client_socket.send(server_answer.encode("utf8"))
+#      client_socket.close()
+#     print(f"Connection with {address[0]}:{address[1]} closed")
 import json
 import socket as s
 import datetime
@@ -26,12 +114,34 @@ def open_user():
 users = open_user()
 
 
-def authenticaion():
-    pass
+def authentication(client_socket):
+    authenticated_user = None
+    while authenticated_user is None:
+        user_request = client_socket.recv(BUFFER).decode("utf8")
+        password_request = client_socket.recv(BUFFER).decode("utf8")
 
+        for user in users:
+            if (
+                user["username"] == user_request
+                and user["password"] == password_request
+            ):
+                authenticated_user = user
+                break
+
+        if authenticated_user:
+            client_socket.send(f"Hi {user_request} !".encode("utf8"))
+            return authenticated_user["type"]
+        else:
+            client_socket.send("Something is wrong, try again".encode("utf8"))
+
+
+authenticated_user_type = authentication(client_socket)
+
+if authenticated_user_type:
+    client_socket.send(f"You are connected as {authenticated_user_type}".encode("utf8"))
+    print(f"You are connected as {authenticated_user_type}")
 
 while True:
-    print(f"Call received from {address[0]}:{address[1]}")
     client_request = client_socket.recv(BUFFER).decode("utf8")
     if client_request == "info":
         server_answer = info
@@ -41,7 +151,7 @@ while True:
         server_answer = str(datetime.datetime.now() - uptime)
     elif client_request == "stop":
         client_socket.send(stop.encode("utf8"))
-        server_socket.close()
+        client_socket.close()
         break
 
     client_socket.send(server_answer.encode("utf8"))
