8a4f1821fea6c3e977b2a14f40e23f8571f81f0d
PioBuczek <piotrbuczek49@gmail.com>
2023-10-14 20:22:21 +0200

    for_fun

--- Progress/for_fun.py
+++ Progress/for_fun.py
@@ -108,11 +108,83 @@
 # 8.Create a set containing unique items from a given list.
 
 
-def unique(list):
-    unique_set = set(list)
-    return unique_set
+# def unique(list):
+#     unique_set = set(list)
+#     return unique_set
 
 
-list = ["something", "is", "bad", "something", "is", "good"]
-result = unique(list)
-print(result)
+# list = ["something", "is", "bad", "something", "is", "good"]
+# result = unique(list)
+# print(result)
+
+
+# 9.Create a Car class with several attributes such as make, year of manufacture, and top speed.
+
+# class Car:
+#     def __init__(self, make, year_of_manufacture, top_speed):
+#         self.make = make
+#         self.year_of_manufacture = year_of_manufacture
+#         self.predkosc = 0
+#         self.top_speed = top_speed
+
+#     def fast(self, top_speed_changing):
+#         if self.predkosc + top_speed_changing <= self.top_speed:
+#             self.predkosc += top_speed_changing
+#         else:
+#             self.predkosc = top_speed_changing
+#         return self.predkosc
+
+
+# car = Car("Mazda", 2010, 120)
+# print(car.fast(100))
+
+
+# 10 Create a text file with a list of names.
+# Write a program that reads these names from a file and sorts them alphabetically.
+
+# with open("Progress\imiona.txt", "r") as file:
+#     data = file.read().splitlines()
+# data.sort()
+# for imie in data:
+#     print(imie)
+
+# 11. Create an abstract Shape class with a calculate_surfaces method.
+# Create classes that inherit from Shape, representing various geometric shapes such as square and triangle.
+
+# from abc import ABC, abstractmethod
+
+
+# class Shape(ABC):
+#     @abstractmethod
+#     def calculate_area(self):
+#         pass
+
+
+# class Triangle(Shape):
+#     def __init__(self, side, height):
+#         self.side = side
+#         self.height = height
+
+#     def calculate_area(self):
+#         return 0.5 * self.height * self.side
+
+
+# class Square(Shape):
+#     def __init__(self, side):
+#         self.side = side
+
+#     def calculate_area(self):
+#         return self.side * self.side
+
+
+# triangle = Triangle(4, 5)
+# square = Square(4)
+
+# area_triangle = triangle.calculate_area()
+# area_square = square.calculate_area()
+
+# print(f"Area of triangle is {area_triangle}")
+# print(f"Area of square is {area_square}")
+
+
+# 12
